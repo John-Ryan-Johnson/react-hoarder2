f10ac05e2681ab6796814535639937a03500ebf4
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-05-09 14:02:15 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Inside React Hoarder</h2>
+        <button className="btn btn-dark">Hoarder</button>
+      </div>
+    );
+  }
+}
+
+export default App;
