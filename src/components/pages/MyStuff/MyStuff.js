7a9b777c9f18cdf0297520239bb71cf5fb9519e3
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-06-04 13:56:55 -0500

    items printing in MyStuff component

--- src/components/pages/MyStuff/MyStuff.js
+++ src/components/pages/MyStuff/MyStuff.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import './MyStuff.scss';
 import { Link } from 'react-router-dom';
+import itemsData from '../../../helpers/data/itemsData';
 
 class MyStuff extends React.Component {
+  state = {
+    items: [],
+  };
+
+  getItems = () => {
+    itemsData
+      .getAllItems()
+      .then((items) => this.setState({ items }))
+      .catch((err) => console.error(err));
+  };
+
+  componentDidMount() {
+    this.getItems();
+  }
+
   render() {
     return (
-      <div className="MyStuff">
-        <h1 className="mt-3 mb-5">My Stuff</h1>
-        <Link className="btn btn-dark mr-5" to="/stuff/12345/edit">Edit</Link>
-        <Link className="btn btn-dark ml-5" to="/stuff/12345">Single</Link>
-      </div>
+      <React.Fragment>
+        <div className='MyStuff'>
+          <h1 className='mt-3 mb-5'>My Stuff</h1>
+          <ul className='list-group mb-5'>
+            {this.state.items.map((item) => (
+              <Link key={item} className='list-group-item'>
+                <h3>{item.itemName}</h3>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/helpers/data/itemsData.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getAllItems = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/items.json`)
+    .then((result) => {
+      const allItemsObj = result.data;
+      const items = [];
+      if (allItemsObj !== null) {
+        Object.keys(allItemsObj).forEach((itemId) => {
+          const newItem = allItemsObj[itemId];
+          newItem.id = itemId;
+          items.push(newItem);
+        });
+      }
+      resolve(items);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+export default { getAllItems };
