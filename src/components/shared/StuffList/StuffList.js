2f5cb78a265ee3d931cb645dda15f7a7d4f6e5cc
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-06-13 12:53:39 -0500

    can edit stuff

--- src/components/shared/StuffList/StuffList.js
+++ src/components/shared/StuffList/StuffList.js
@@ -14,10 +14,13 @@ class StuffList extends React.Component {
   render() {
     const { item, removeItem } = this.props;
     const singleLink = `/stuff/${item.id}`;
+    const editLink = `/edit/${item.id}`;
+
     return (
       <div className="StuffList">
         <li className="list-group-item">
           <Link to={singleLink}><h3 className="item-name">{item.itemName}</h3></Link>
+          <Link className="btn btn-warning" to={editLink}><i class="fas fa-pencil-alt"></i></Link>
           <button className="btn btn-dark" onClick={() => removeItem(item.id)}><i className="far fa-trash-alt"></i></button>
         </li>
       </div>
